b45ff983c242464b681e20568ead30427e5b8475
Jordy500 <156438783+Jordy500@users.noreply.github.com>
2024-01-19 15:07:02 +0100

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,102 +1,32 @@
-class OthelloBot:
-    def __init__(self, board):
-        self.board = board  # Suppose that board is a 2D list representing the game state
-        self.my_color = "X"  # Or "O" depending on the bot's color
-        self.opponent_color = "O" if self.my_color == "X" else "X"
+# À l'intérieur de la classe Bot:
 
-    def is_corner(self, x, y):
-        return (x, y) in [(0, 0), (0, 7), (7, 0), (7, 7)]
+# ...
+
+def check_valid_moves(self, board, color):
+    # ... (le reste du code précédent)
     
-    def get_valid_moves(self):
-        # Your existing method to get the list of valid moves
-        # Should return a list of tuples (x, y) representing valid move coordinates
-        pass
+    # Ajouter une vérification pour les coins pour augmenter leur priorité
+    for move in valid_moves:
+        x_pos, y_pos = move[0], move[1]
+        if (x_pos, y_pos) in [(0, 0), (0, 7), (7, 0), (7, 7)]:
+            move.append(1000)  # Ajouter un poids élevé pour les coins
+        else:
+            move.append(new_board.board[tile_index].weight)
+    
+    # ... (le reste du code précédent)
 
-    def move_gives_corner(self, x, y):
-        # Check if the move at (x, y) would allow the opponent to take a corner
-        # This requires game logic to see if placing a piece gives access to a corner
-        pass
+def calculate_mobility(self, board, color):
+    # Calcule le nombre de mouvements légaux après un coup
+    mobility = 0
+    for x in range(8):
+        for y in range(8):
+            if board.is_legal_move(x, y, color):
+                mobility += 1
+    return mobility
 
-    def select_move(self):
-        valid_moves = self.get_valid_moves()
-        corner_moves = [move for move in valid_moves if self.is_corner(*move)]
-        
-        if corner_moves:
-            return corner_moves[0]  # Prioritize taking a corner if it's safe
-        
-        # Filter out moves that give the opponent a corner
-        non_risky_moves = [move for move in valid_moves if not self.move_gives_corner(*move)]
-        
-        if non_risky_moves:
-            # Select the best move from non-risky moves based on your strategy
-            # For simplicity, just return the first non-risky move
-            return non_risky_moves[0]
-        
-        # If there are no non-risky moves, select the least damaging move
-        return valid_moves[0] if valid_moves else None
+# ...
 
-# Usage
-# board = ... (initialize or get the current state of the board)
-# bot = OthelloBot(board)
-# move = bot.select_move()
-# if move:
-#     x, y = move
-#     # Make the move on the board
-
-
-
-class OthelloBot:
-    def __init__(self, color):
-        self.color = color  # La couleur du bot, soit 'B' soit 'W'
-
-    def is_valid_move(self, move, board):
-        # Implémentation d'une fonction qui vérifie si le coup est valide
-        # Cette fonction doit vérifier que le coup est légal selon les règles d'Othello
-        pass
-
-    def evaluate_move(self, move, board):
-        # Évalue la qualité du coup
-        x, y = move
-        if (x, y) in [(0, 0), (0, 7), (7, 0), (7, 7)]:
-            # Le coup est un coin, donc très avantageux
-            return 100
-        if x == 0 or x == 7 or y == 0 or y == 7:
-            # Le coup est sur un bord mais pas un coin, donc potentiellement risqué
-            return -10
-        return 0  # Les autres coups sont neutres pour cette démonstration
-
-    def select_move(self, board):
-        valid_moves = self.get_all_valid_moves(board)
-        best_move = None
-        best_score = -float('inf')
-        
-        for move in valid_moves:
-            if self.is_corner(move):
-                return move  # Priorité aux coins
-            score = self.evaluate_move(move, board)
-            if score > best_score:
-                best_score = score
-                best_move = move
-        
-        # Évite les bords A, H, 1, et 8 au début du jeu
-        if best_move and (best_move[0] in [0, 7] or best_move[1] in [0, 7]):
-            if self.is_early_game(board):
-                return None  # Indique qu'aucun mouvement n'est choisi
-            else:
-                return best_move
-        
-        return best_move
-
-    def is_corner(self, move):
-        # Vérifie si le coup est dans un coin
-        return move in [(0, 0), (0, 7), (7, 0), (7, 7)]
-
-    def is_early_game(self, board):
-        # Définit si le jeu est au début en fonction du nombre de pièces sur le plateau
-        return sum(sum(1 for cell in row if cell) for row in board) < 20
-
-    def get_all_valid_moves(self, board):
-        # Retourne une liste de tous les coups valides pour le bot
-        # Cette fonction doit générer tous les mouvements possibles et vérifier leur validité
-        pass
-
+# Lors de la sélection du mouvement:
+# Ajoutez un poids basé sur la mobilité
+if total_flipped + mobility_weight * self.calculate_mobility(board, color) > max_pawns_flipped:
+    # ... (le reste du code précédent)
