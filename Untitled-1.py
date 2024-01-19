df558b5a9a6edeae110d3d3963e1d078c6239105
Jordy500 <156438783+Jordy500@users.noreply.github.com>
2024-01-19 14:50:56 +0100

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -42,3 +42,61 @@ class OthelloBot:
 # if move:
 #     x, y = move
 #     # Make the move on the board
+
+
+
+class OthelloBot:
+    def __init__(self, color):
+        self.color = color  # La couleur du bot, soit 'B' soit 'W'
+
+    def is_valid_move(self, move, board):
+        # Implémentation d'une fonction qui vérifie si le coup est valide
+        # Cette fonction doit vérifier que le coup est légal selon les règles d'Othello
+        pass
+
+    def evaluate_move(self, move, board):
+        # Évalue la qualité du coup
+        x, y = move
+        if (x, y) in [(0, 0), (0, 7), (7, 0), (7, 7)]:
+            # Le coup est un coin, donc très avantageux
+            return 100
+        if x == 0 or x == 7 or y == 0 or y == 7:
+            # Le coup est sur un bord mais pas un coin, donc potentiellement risqué
+            return -10
+        return 0  # Les autres coups sont neutres pour cette démonstration
+
+    def select_move(self, board):
+        valid_moves = self.get_all_valid_moves(board)
+        best_move = None
+        best_score = -float('inf')
+        
+        for move in valid_moves:
+            if self.is_corner(move):
+                return move  # Priorité aux coins
+            score = self.evaluate_move(move, board)
+            if score > best_score:
+                best_score = score
+                best_move = move
+        
+        # Évite les bords A, H, 1, et 8 au début du jeu
+        if best_move and (best_move[0] in [0, 7] or best_move[1] in [0, 7]):
+            if self.is_early_game(board):
+                return None  # Indique qu'aucun mouvement n'est choisi
+            else:
+                return best_move
+        
+        return best_move
+
+    def is_corner(self, move):
+        # Vérifie si le coup est dans un coin
+        return move in [(0, 0), (0, 7), (7, 0), (7, 7)]
+
+    def is_early_game(self, board):
+        # Définit si le jeu est au début en fonction du nombre de pièces sur le plateau
+        return sum(sum(1 for cell in row if cell) for row in board) < 20
+
+    def get_all_valid_moves(self, board):
+        # Retourne une liste de tous les coups valides pour le bot
+        # Cette fonction doit générer tous les mouvements possibles et vérifier leur validité
+        pass
+
