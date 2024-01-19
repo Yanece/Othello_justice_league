69f289a9c44dfb5e8573350e114e58e411a0e0c0
Jordy500 <156438783+Jordy500@users.noreply.github.com>
2024-01-19 15:31:48 +0100

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,31 +1,30 @@
-# À l'intérieur de la classe Bot:
-# ...
+class Bot:
+    # ...
 
-def check_valid_moves(self, board, color):
-    # ... (le reste du code précédent)
-    
-    # Ajouter une vérification pour les coins pour augmenter leur priorité
-    for move in valid_moves:
-        x_pos, y_pos = move[0], move[1]
-        if (x_pos, y_pos) in [(0, 0), (0, 7), (7, 0), (7, 7)]:
-            move.append(1000)  # Ajouter un poids élevé pour les coins
-        else:
-            move.append(new_board.board[tile_index].weight)
-    
-    # ... (le reste du code précédent)
+    def check_valid_moves(self, board, color):
+        # ... (le reste du code précédent)
 
-def calculate_mobility(self, board, color):
-    # Calcule le nombre de mouvements légaux après un coup
-    mobility = 0
-    for x in range(8):
-        for y in range(8):
-            if board.is_legal_move(x, y, color):
-                mobility += 1
-    return mobility
+        # Ajouter une vérification pour les coins pour augmenter leur priorité
+        corner_positions = [(0, 0), (0, 7), (7, 0), (7, 7)]
+        for move in valid_moves:
+            x_pos, y_pos = move[0], move[1]
+            if (x_pos, y_pos) in corner_positions:
+                move.append(1000)  # Ajouter un poids élevé pour les coins
+            else:
+                move.append(self.calculate_mobility(board, color))  # Utiliser la mobilité pour pondérer le mouvement
+        
+        # Choisissez le meilleur coup en fonction du poids total (pions retournés + poids du coin/mobilité)
+        best_move = max(valid_moves, key=lambda x: x[2] + x[3])
 
-# ...
+        return best_move[:2]  # Retourner uniquement les coordonnées x, y
 
-# Lors de la sélection du mouvement:
-# Ajoutez un poids basé sur la mobilité
-if total_flipped + mobility_weight * self.calculate_mobility(board, color) > max_pawns_flipped:
-    # ... (le reste du code précédent)
+    def calculate_mobility(self, board, color):
+        # Calcule le nombre de mouvements légaux après un coup
+        mobility = 0
+        for x in range(8):
+            for y in range(8):
+                if board.is_legal_move(x, y, color):
+                    mobility += 1
+        return mobility
+
+    # ... (le reste de la classe Bot)
