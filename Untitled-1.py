dfe312e4e9d2badf0e20c18454186f80677bc9aa
Jordy500 <156438783+Jordy500@users.noreply.github.com>
2024-01-19 15:47:49 +0100

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -28,3 +28,51 @@ class Bot:
         return mobility
 
     # ... (le reste de la classe Bot)
+
+
+class Bot:
+    def __init__(self, name):
+        self.name = name
+    
+    def check_valid_moves(self, board, color):
+        valid_moves = []
+        max_score = -999
+        best_move = None
+        
+        for x in range(8):
+            for y in range(8):
+                if board.is_tile_empty(x, y) and board.is_legal_move(x, y, color):
+                    score = self.evaluate_move(board, x, y, color)
+                    if score > max_score:
+                        max_score = score
+                        best_move = (x, y)
+                    elif score == max_score and best_move is not None:
+                        best_move = random.choice([(x, y), best_move])
+        
+        return best_move
+
+    def evaluate_move(self, board, x, y, color):
+        score = 0
+        corners = [(0, 0), (0, 7), (7, 0), (7, 7)]
+        
+        # Score for capturing corners
+        if (x, y) in corners:
+            score += 100
+        
+        # Score for mobility
+        score += self.calculate_mobility(board, color)
+        
+        # Score for flipping opponent's tiles
+        flipping_score = len(board.get_flipped_tiles(x, y, color))
+        score += flipping_score
+        
+        return score
+
+    def calculate_mobility(self, board, color):
+        mobility = 0
+        for x in range(8):
+            for y in range(8):
+                if board.is_legal_move(x, y, color):
+                    mobility += 1
+        return mobility
+
